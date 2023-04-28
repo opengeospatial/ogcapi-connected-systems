da1cb918d8db60b84c7a8a1e610da329f94760d1
Alex Robin <alex.robin@sensiasoft.com>
2023-04-28 17:14:32 +0200

    [API] Migration of spec to Metanorma (WIP)

--- api/part2/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-asciidoctor -a data-uri -o standard_document.html standard/standard_document.adoc
-
-#asciidoctor-pdf --safe -o standard_document.pdf standard_document.adoc
---
+++ api/part2/standard/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+metanorma compile --agree-to-terms -t ogc -x html 23-002r0.adoc
